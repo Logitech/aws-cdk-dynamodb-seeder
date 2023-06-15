003db9ca2fe4238b7d29e21c081f0fbef23005e3
Tomek Marcinkowski <tmarcinkowski@logitech.com>
2023-06-15 13:31:01 +0200

    feat!: migrated to AWS CDK v2

--- tests/seeder.ts
+++ tests/seeder.ts
@@ -1,53 +1,56 @@
-import { Stack } from '@aws-cdk/core';
-import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
-import '@aws-cdk/assert/jest';
-
+import { Stack } from 'aws-cdk-lib';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
+import { Template } from 'aws-cdk-lib/assertions';
 import { Seeder } from '../lib/index';
 
-it('seeds a table from required json files', () => {
-  const stack = new Stack();
-  new Seeder(stack, 'Seeder', {
-    table: new Table(stack, 'TestTable', {
-      tableName: 'TestTable',
-      partitionKey: { name: 'Id', type: AttributeType.STRING },
-    }),
-    setup: require('./put.json'),
-    teardown: require('./delete.json'),
-    refreshOnUpdate: true,
+describe('seeder', () => {
+  it('seeds a table from required json files', () => {
+    const stack = new Stack();
+    new Seeder(stack, 'Seeder', {
+      table: new Table(stack, 'TestTable', {
+        tableName: 'TestTable',
+        partitionKey: { name: 'Id', type: AttributeType.STRING },
+      }),
+      setup: require('./put.json'),
+      teardown: require('./delete.json'),
+      refreshOnUpdate: true,
+    });
+
+    const template = Template.fromStack(stack);
+    template.hasResource('AWS::Lambda::Function', {});
+    template.hasResource('AWS::S3::Bucket', {});
   });
 
-  expect(stack).toHaveResource('AWS::Lambda::Function');
-  expect(stack).toHaveResource('AWS::S3::Bucket');
-});
-
-it('seeds a table from inline arrays', () => {
-  const stack = new Stack();
-  new Seeder(stack, 'Seeder', {
-    table: new Table(stack, 'TestTable', {
-      tableName: 'TestTable',
-      partitionKey: { name: 'Id', type: AttributeType.STRING },
-    }),
-    setup: [
-      {
-        id: 'herewego...',
-        this: 'is a test',
-        testing: {
-          testing: 123,
+  it('seeds a table from inline arrays', () => {
+    const stack = new Stack();
+    new Seeder(stack, 'Seeder', {
+      table: new Table(stack, 'TestTable', {
+        tableName: 'TestTable',
+        partitionKey: { name: 'Id', type: AttributeType.STRING },
+      }),
+      setup: [
+        {
+          id: 'herewego...',
+          this: 'is a test',
+          testing: {
+            testing: 123,
+          },
         },
-      },
-      {
-        id: 'greatest show',
-        this: 'is a the greatest show',
-      },
-    ],
-    teardown: [
-      {
-        id: 'greatest show',
-      },
-    ],
-    refreshOnUpdate: true,
-  });
+        {
+          id: 'greatest show',
+          this: 'is a the greatest show',
+        },
+      ],
+      teardown: [
+        {
+          id: 'greatest show',
+        },
+      ],
+      refreshOnUpdate: true,
+    });
 
-  expect(stack).toHaveResource('AWS::Lambda::Function');
-  expect(stack).toHaveResource('AWS::S3::Bucket');
+    const template = Template.fromStack(stack);
+    template.hasResource('AWS::Lambda::Function', {});
+    template.hasResource('AWS::S3::Bucket', {});
+  });
 });
